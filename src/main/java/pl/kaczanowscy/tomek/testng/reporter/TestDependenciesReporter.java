7206a0c533447562aa43d8e59a68c4b11bee11d4
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2011-01-23 18:03:49 -0600

    prints a lot of debug in dot file

--- src/main/java/pl/kaczanowscy/tomek/testng/reporter/TestDependenciesReporter.java
+++ src/main/java/pl/kaczanowscy/tomek/testng/reporter/TestDependenciesReporter.java
@@ -2,36 +2,82 @@ package pl.kaczanowscy.tomek.testng.reporter;
 
 import org.testng.IReporter;
 import org.testng.ISuite;
+import org.testng.ITestNGMethod;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlInclude;
 import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class TestDependenciesReporter implements org.testng.IReporter {
 
     // FIXME should rather use outputDirectory injected by TestNG, see method parameters below
     public final static String DEFAULT_OUTPUT_DIR = "build/reports/test-dependencies";
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private static final String DEPENDS_UPON = " -> ";
 
 
     public void generateReport(java.util.List<XmlSuite> xmlSuites, java.util.List<ISuite> suites, java.lang.String outputDirectory) {
 
-        File blah = new File(DEFAULT_OUTPUT_DIR + "/blah.dot");
+        File blah = new File(DEFAULT_OUTPUT_DIR + "/dotFile.dot");
+        Writer out = null;
         try {
-            FileWriter writer = new FileWriter(blah);
+            out = new OutputStreamWriter(new FileOutputStream(blah));
+            out.write("digraph testDependencies {"  + NEWLINE);
+            // TODO
             for (ISuite suite : suites) {
-                        System.out.println("isuite: " + suite.getName());
-                        writer.write("isuite: " + suite.getName());
+                System.out.println("isuite: " + suite.getName());
+                out.write("isuite: " + suite.getName()  + NEWLINE);
+                for (Map.Entry<String,Collection<ITestNGMethod>> entry : suite.getMethodsByGroups().entrySet()) {
+                    out.write("entry: " + entry.getKey() + NEWLINE);
+                    for (ITestNGMethod method :  entry.getValue()) {
+                        out.write("\tmethod: " + method.getMethodName() + NEWLINE);
+                        for (String dependedUponMethod : method.getMethodsDependedUpon()) {
+                            out.write(method.getMethodName() + DEPENDS_UPON + dependedUponMethod.substring(dependedUponMethod.lastIndexOf(".")+1) + NEWLINE);
+                            //out.write("dep upon method: " + dependedUponMethod + NEWLINE);
+                            //String[] tokens = dependedUponMethod.split("\\.");
+                            //out.write(Arrays.deepToString(tokens));
+                            //out.write(method.getMethodName() + DEPENDS_UPON + tokens[tokens.length-2] + "." + tokens[tokens.length-1]);
+                        }
                     }
+                }
+            }
 
 
-        for (XmlSuite suite : xmlSuites) {
-            System.out.println("xmlsuite: " + suite);
-            writer.write("xmlsuite: " + suite);
-                 }
-            writer.close();
+            for (XmlSuite suite : xmlSuites) {
+                System.out.println("xmlsuite: " + suite);
+                out.write("xmlsuite: " + suite + NEWLINE);
+                List<XmlTest> tests = suite.getTests();
+                for (XmlTest test: tests) {
+                    out.write("test: " + test + NEWLINE);
+                    for (XmlClass clazz : test.getXmlClasses()) {
+                        out.write("clazz: " + clazz + NEWLINE);
+                        for (XmlInclude include : clazz.getIncludedMethods()) {
+                            out.write("include: " + include + NEWLINE);
+                        }
+                    }
+                }
+            }
+            out.write("}");
+
+            // FIXME exception handling
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                // FIXME
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
         }
     }
 
--- src/test/java/pl/kaczanowscy/tomek/test/TestDependenciesReporterTest.java
+++ src/test/java/pl/kaczanowscy/tomek/test/TestDependenciesReporterTest.java
@@ -1,33 +1,88 @@
 package pl.kaczanowscy.tomek.test;
 
 import org.testng.TestNG;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.kaczanowscy.tomek.testng.reporter.TestDependenciesReporter;
 import pl.kaczanowscy.tomek.testng.reporter.tests.TestA;
 import pl.kaczanowscy.tomek.testng.reporter.tests.TestB;
 import pl.kaczanowscy.tomek.testng.reporter.tests.TestC;
 
-import java.io.File;
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static org.testng.Assert.assertTrue;
 
 @Test
 public class TestDependenciesReporterTest {
 
-	public void testReporter() {
-		TestNG testNG = new TestNG();
-        //testNG.setTestJar("bab");
-        testNG.setTestClasses(new Class[] {TestA.class, TestB.class, TestC.class});
-        testNG.setListenerClasses(Arrays.asList(new Class[] {TestDependenciesReporter.class}));
+    // TODO temp files instead?
+
+    private TestNG testNG;
+
+    @BeforeMethod
+    public void setUp() {
+        testNG = new TestNG();
+        testNG.setListenerClasses(Arrays.asList(new Class[]{TestDependenciesReporter.class}));
         testNG.setOutputDirectory(TestDependenciesReporter.DEFAULT_OUTPUT_DIR);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        removeOutputDir();
+    }
+
+    public void testOneTestClass() throws FileNotFoundException {
+        testNG.setTestClasses(new Class[] {TestA.class});
+        testNG.run();
+
+        String text = readDotFile();
+        assertTrue(text.contains("digraph testDependencies {"), text);
+        assertTrue(text.contains("test_A_1"), text);
+        assertTrue(text.contains("test_A_2"), text);
+        assertTrue(text.contains("test_A_3"), text);
+
+        assertTrue(text.contains("test_A_2 -> test_A_1"), text);
+        assertTrue(text.contains("test_A_3 -> test_A_2"), text);
+        assertTrue(text.contains("}"), text);
+    }
+
+    private String readDotFile() throws FileNotFoundException {
+        File dotFile = new File(TestDependenciesReporter.DEFAULT_OUTPUT_DIR + "/dotFile.dot");
+        assertTrue(dotFile.exists());
+
+        return readFile(dotFile);
+    }
+
+   /* public void testThreeTestClasses() throws FileNotFoundException {
+        //testNG.setTestJar("bab");
+        testNG.setTestClasses(new Class[]{TestA.class, TestB.class, TestC.class});
         testNG.run();
 
         // FIXME test stuff here
-        File blah = new File(TestDependenciesReporter.DEFAULT_OUTPUT_DIR + "/blah.dot");
-        assertTrue(blah.exists());
+        String text = readDotFile();
+    }*/
 
-        blah = new File(TestDependenciesReporter.DEFAULT_OUTPUT_DIR + "/blah2.dot");
-        assertTrue(blah.exists());
-	}
+    private String readFile(File blah) throws FileNotFoundException {
+        StringBuilder text = new StringBuilder();
+        String NL = System.getProperty("line.separator");
+        Scanner scanner = new Scanner(new FileInputStream(blah));
+        try {
+          while (scanner.hasNextLine()){
+            text.append(scanner.nextLine() + NL);
+          }
+        }
+        finally{
+          scanner.close();
+        }
+        return text.toString();
+    }
+
+    private void removeOutputDir() {
+        File outputDir = new File(TestDependenciesReporter.DEFAULT_OUTPUT_DIR);
+        outputDir.delete();
+    }
+
 }
