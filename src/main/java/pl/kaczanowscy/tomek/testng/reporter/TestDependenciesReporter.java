dd58ca570fb1be69904d709385cc7551919d737e
Tomek Kaczanowski <tomekkaczanowski@o2.pl>
2011-01-25 23:42:30 -0600

    executes dot to draw a graph

--- src/main/java/pl/kaczanowscy/tomek/testng/reporter/TestDependenciesReporter.java
+++ src/main/java/pl/kaczanowscy/tomek/testng/reporter/TestDependenciesReporter.java
@@ -24,19 +24,19 @@ public class TestDependenciesReporter implements org.testng.IReporter {
 
     public void generateReport(java.util.List<XmlSuite> xmlSuites, java.util.List<ISuite> suites, java.lang.String outputDirectory) {
 
-        File blah = new File(DEFAULT_OUTPUT_DIR + "/dotFile.dot");
+        File dotFile = new File(DEFAULT_OUTPUT_DIR + "/dotFile.dot");
         Writer out = null;
         try {
-            out = new OutputStreamWriter(new FileOutputStream(blah));
+            out = new OutputStreamWriter(new FileOutputStream(dotFile));
             out.write("digraph testDependencies {"  + NEWLINE);
-            // TODO
+            out.write("  rankdir=BT;" + NEWLINE);
             for (ISuite suite : suites) {
-                System.out.println("isuite: " + suite.getName());
-                out.write("isuite: " + suite.getName()  + NEWLINE);
+                //System.out.println("isuite: " + suite.getName());
+                // out.write("isuite: " + suite.getName()  + NEWLINE);
                 for (Map.Entry<String,Collection<ITestNGMethod>> entry : suite.getMethodsByGroups().entrySet()) {
-                    out.write("entry: " + entry.getKey() + NEWLINE);
+                    //out.write("entry: " + entry.getKey() + NEWLINE);
                     for (ITestNGMethod method :  entry.getValue()) {
-                        out.write("\tmethod: " + method.getMethodName() + NEWLINE);
+                        //out.write("\tmethod: " + method.getMethodName() + NEWLINE);
                         for (String dependedUponMethod : method.getMethodsDependedUpon()) {
                             out.write(method.getMethodName() + DEPENDS_UPON + dependedUponMethod.substring(dependedUponMethod.lastIndexOf(".")+1) + NEWLINE);
                             //out.write("dep upon method: " + dependedUponMethod + NEWLINE);
@@ -51,14 +51,14 @@ public class TestDependenciesReporter implements org.testng.IReporter {
 
             for (XmlSuite suite : xmlSuites) {
                 System.out.println("xmlsuite: " + suite);
-                out.write("xmlsuite: " + suite + NEWLINE);
+                //out.write("xmlsuite: " + suite + NEWLINE);
                 List<XmlTest> tests = suite.getTests();
                 for (XmlTest test: tests) {
-                    out.write("test: " + test + NEWLINE);
+                    //out.write("test: " + test + NEWLINE);
                     for (XmlClass clazz : test.getXmlClasses()) {
-                        out.write("clazz: " + clazz + NEWLINE);
+                        //out.write("clazz: " + clazz + NEWLINE);
                         for (XmlInclude include : clazz.getIncludedMethods()) {
-                            out.write("include: " + include + NEWLINE);
+                            //out.write("include: " + include + NEWLINE);
                         }
                     }
                 }
@@ -70,14 +70,36 @@ public class TestDependenciesReporter implements org.testng.IReporter {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        finally {
+        } finally {
             try {
                 out.close();
             } catch (IOException e) {
                 // FIXME
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
+        }
+
+
+        generateDiagram(dotFile);
+
+    }
+
+    // TODO read parameters (e.g. output format) from properties
+    private void generateDiagram(File dotFile) {
+        try {
+            Runtime run = Runtime.getRuntime() ;
+            Process pr =  run.exec("dot " + dotFile.getAbsolutePath() + " -Tpng -ograph.png");
+            pr.waitFor() ;
+            BufferedReader buf = new BufferedReader( new InputStreamReader( pr.getInputStream() ) ) ;
+            String line;
+            while ( ( line = buf.readLine() ) != null )
+            {
+                System.out.println(line) ;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
 
